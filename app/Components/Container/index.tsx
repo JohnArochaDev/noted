d22f5de101447a8265be47c26f5fb8557df92fd4
JohnDev <jparocha777@gmail.com>
2025-12-29 12:43:29 -0700

    fixed canvas and button styling

--- app/Components/Container/index.tsx
+++ app/Components/Container/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import styes from './styles.module.scss'
+import styles from './styles.module.scss'
 
 export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <div className={styes.button}>{children}</div>;
+  return <div className={styles.container}>{children}</div>;
 };
