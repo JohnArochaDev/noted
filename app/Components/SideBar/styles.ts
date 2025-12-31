ce0843dfc764e882e7dcb80cd505789d2af58365
JohnDev <jparocha777@gmail.com>
2025-12-30 17:38:16 -0700

    renamed colors

--- app/Components/SideBar/styles.ts
+++
@@ -1,9 +0,0 @@
-import { lighterBackground, lighterTrim } from "@/app/Constants/colors";
-
-export const SideBarStyling = {
-  backgroundColor: lighterBackground,
-  width: "250px",
-  border: `2px solid ${lighterTrim}`,
-  display: "flex",
-  flexDirection: "column" as const,
-};
