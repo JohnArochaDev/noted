515ddffe4d1802a7de2e215676be8c8697e7c5ed
JohnDev <jparocha777@gmail.com>
2026-01-01 18:33:08 -0700

    added styling to the three dots

--- app/Components/SideBar/ThreeDots/index.tsx
+++ app/Components/SideBar/ThreeDots/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Image from "next/image";
 
 import styles from "./styles.module.scss";
@@ -8,15 +10,34 @@ type ThreeDotsType = {
 
 export const ThreeDots = (props: ThreeDotsType) => {
   const { isHovered } = props;
+  const [imageSrc, setImageSrc] = useState("/assets/threeDots.png");
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    setImageSrc("/assets/threeDotsSelected.png");
+  };
+
+  const handleMouseUp = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    setImageSrc("/assets/threeDots.png");
+  };
+
   return (
-    <div className={styles.threeDots}>
+    <div className={styles.threeDots} onClick={(e) => e.stopPropagation()}>
       {isHovered && (
-        <Image
-          src="/assets/threeDots.png"
-          alt="Closed Folder Icon"
-          width={22}
-          height={22}
-        />
+        <div
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          onMouseLeave={handleMouseUp}
+          style={{ cursor: "pointer" }}
+        >
+          <Image
+            src={imageSrc}
+            alt="Closed Folder Icon"
+            width={22}
+            height={22}
+          />
+        </div>
       )}
     </div>
   );
