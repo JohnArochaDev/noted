708b13e637d3315edc73984984193f9aa69b7127
JohnDev <jparocha777@gmail.com>
2026-01-07 17:51:01 -0700

    createed initial login modal

--- app/Components/Login/index.tsx
+++ app/Components/Login/index.tsx
@@ -3,32 +3,8 @@ import styles from "./styles.module.scss";
 
 export const LoginPage = () => {
   return (
-    <div className={styles.loginScreen}>
-      <div
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          color: "white",
-          fontSize: "2rem",
-          textAlign: "center",
-          zIndex: 10,
-        }}
-      >
-        <h1>Welcome to Noted</h1>
-        <p>Please log in to continue</p>
-        {/* Add your login form here later */}
-        <button
-          style={{
-            padding: "10px 20px",
-            fontSize: "1.2rem",
-            marginTop: "20px",
-          }}
-        >
-          Log In (coming soon)
-        </button>
-      </div>
+    <div className={styles.login}>
+      <div className={styles.loginModal}></div>
     </div>
   );
 };
