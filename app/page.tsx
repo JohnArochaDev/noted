3701f7085c54dc1472086c97c05816ff99c5c511
JohnDev <jparocha777@gmail.com>
2026-01-09 20:26:56 -0700

    fixed small bug with useRouter and Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 
 import { ReactFlowProvider } from "@xyflow/react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 import { Canvas } from "./Components/Canvas";
 import { Container } from "./Components/Container";
 import { HotBar } from "./Components/HotBar";
 import { LoggedInSwitch } from "./Components/LoggedInSwitch";
 import { LoginPage } from "./Components/Login";
-import { SearchParamsHandler } from "./Components/SearchParamsHandler";
 import { SideBar } from "./Components/SideBar";
 import { TreeBuilder } from "./Components/SideBar/TreeBuilder";
 import { ToastProvider } from "./Components/Toast";
@@ -17,32 +17,41 @@ import { NodeProvider } from "./Context";
 import styles from "./styles.module.scss";
 
 const Home = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  // removes all search params on page refresh
+  useEffect(() => {
+    if (searchParams.size > 0) {
+      router.replace(pathname, { scroll: false });
+    }
+  }, [searchParams, pathname, router]); // ← Added dependencies to satisfy ESLint
+
   return (
     <div className={styles.pageWrapper}>
-      <Suspense fallback={<div>Loading...</div>}>
-        {" "}
-        <SearchParamsHandler />
+      <Suspense fallback={<></>}>
+        <NodeProvider>
+          <ToastProvider>
+            <LoggedInSwitch
+              loggedIn={
+                <>
+                  <HotBar />
+                  <Container>
+                    <SideBar>
+                      <TreeBuilder />
+                    </SideBar>
+                    <ReactFlowProvider>
+                      <Canvas />
+                    </ReactFlowProvider>
+                  </Container>
+                </>
+              }
+              loggedOut={<LoginPage />}
+            />
+          </ToastProvider>
+        </NodeProvider>
       </Suspense>
-      <NodeProvider>
-        <ToastProvider>
-          <LoggedInSwitch
-            loggedIn={
-              <>
-                <HotBar />
-                <Container>
-                  <SideBar>
-                    <TreeBuilder />
-                  </SideBar>
-                  <ReactFlowProvider>
-                    <Canvas />
-                  </ReactFlowProvider>
-                </Container>
-              </>
-            }
-            loggedOut={<LoginPage />}
-          />
-        </ToastProvider>
-      </NodeProvider>
     </div>
   );
 };
