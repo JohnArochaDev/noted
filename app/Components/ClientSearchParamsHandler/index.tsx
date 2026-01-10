5d5e877e6b14fa8ca9fe01a240357f239e582c28
JohnDev <jparocha777@gmail.com>
2026-01-09 21:31:13 -0700

    checking netlify rsc calls

--- app/Components/ClientSearchParamsHandler/index.tsx
+++ app/Components/ClientSearchParamsHandler/index.tsx
@@ -1,24 +1,24 @@
 "use client";
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-import { usePathname, useRouter } from "next/navigation";
+// import { usePathname, useRouter } from "next/navigation";
 
 type Props = {
   searchParams: Record<string, string | string[] | undefined>;
 };
 
 export default function ClientSearchParamsHandler({ searchParams }: Props) {
-  const router = useRouter();
-  const pathname = usePathname();
+  // const router = useRouter();
+  // const pathname = usePathname();
 
-  useEffect(() => {
-    // Convert searchParams to URLSearchParams for size check
-    const params = new URLSearchParams(searchParams as Record<string, string>);
-    if (params.size > 0) {
-      router.replace(pathname, { scroll: false });
-    }
-  }, [searchParams, pathname, router]);
+  // useEffect(() => {
+  //   // Convert searchParams to URLSearchParams for size check
+  //   const params = new URLSearchParams(searchParams as Record<string, string>);
+  //   if (params.size > 0) {
+  //     router.replace(pathname, { scroll: false });
+  //   }
+  // }, [searchParams, pathname, router]);
 
   return null; // Invisible
 }
